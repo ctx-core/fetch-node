cf5df3e165f04effc3f8eba1d4567dc31de19945
Brian Takita <brian.takita@gmail.com>
2022-07-19 00:42:09 -0500

    update dependencies

--- src/fetch_response_pair_.js
+++ src/fetch_response_pair_.js
@@ -1,4 +1,4 @@
 import { fetch_response_pair_2 } from '@ctx-core/fetch'
 import { fetch } from './fetch.js'
 /** @type {import('@ctx-core/fetch').fetch_response_pair_} */
-export const fetch_response_pair_ = fetch_response_pair_2(fetch)
+export const fetch_response_pair_ = fetch_response_pair_2(/** @type {typeof fetch} */fetch)
